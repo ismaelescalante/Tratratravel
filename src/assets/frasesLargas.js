a6dbc4e7724719f961f76d30264f053fb0682fff
Fulgencio <funche_em@hotmail.com>
2023-01-26 09:51:28 +0100

    frasesLargas en carpeta assets

---
+++ src/assets/frasesLargas.js
@@ -0,0 +1,26 @@
+
+
+
+  export const FrasesSabiasQue = [
+    "¿Sabías que explorar el mundo puede mejorar tu salud mental?",
+    "¿Sabías que conocer gente nueva puede ser una experiencia increíble?",
+    "¿Sabías que los viajes tienen el poder de abrir tu mente a nuevas perspectivas?",
+    "¿Sabías que descubrir nuevos horizontes te ayuda a superar tus límites?",
+    "¿Sabías que explorar el mundo puede ser la mejor terapia para el alma?",
+    "¿Sabías que los viajes te permiten conocerte mejor a ti mismo?",
+    "¿Sabías que la aventura es una gran manera de aprender y desarrollar nuevas habilidades?",
+    "¿Sabías que conocer culturas diferentes te brindan nuevas ideas?",
+    "¿Sabías que los viajes te ofrecen la oportunidad de experimentar la magia del mundo?",
+    "¿Sabías que los viajes te ayudan a romper con la monotonía y experimentar algo nuevo?",
+    "¿Sabías que descubrir nuevos lugares te inspira a ver las cosas de una manera positiva?",
+    "¿Sabías que descubrir nuevos países te ayuda a comprender sus diferencias?",
+    "¿Sabías que la aventura es una manera entretenida y emocionante de relajarte y liberar tu estrés?",
+    "¿Sabías que explorar el mundo te permite expandir tus horizontes?",
+    "¿Sabías que descubrir nuevos destinos te ayuda a expandir tu conocimiento sobre el mundo?",
+    "¿Sabías que conocer nuevos lugares te brinda la oportunidad de experimentar cosas nuevas?",
+    "¿Sabías que el viaje puede ser una maravillosa forma de vivir la vida?",
+    "¿Sabías que los viajes te permiten mejorar y desarrollar tus propias habilidades?",
+    "¿Sabías que los viajes son una gran manera de estimular tu curiosidad?",
+    "¿Sabías que los viajes te ayudan a encontrar tu verdadero propósito en la vida?"
+  ]
+  ;
