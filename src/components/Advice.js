7ffd0445ae6614134e21914665cfda543991dccf
Fulgencio <funche_em@hotmail.com>
2023-01-31 15:52:39 +0100

    imagenes dinámicas y widget funcional

--- src/components/Advice.js
+++ src/components/Advice.js
@@ -22,7 +22,7 @@ const Advice = () => {
     
     <section>
 {<div className="searchWidget" style={{width:"300px", height:"600px"}}>
-  <iframe id="widgetIframe" src="https://www.expedia.com/marketing/widgets/searchform/widget?wtt=5&tp1=tratratravel&tp2=tratratravel&lob=H,FH,F,CA,A&des=&wbi=666699&olc=FFFFFF&whf=4&hfc=FFFFFF&wif=4&ifc=&wbc=286078&wbf=4&bfc=FFFFFF&wws=2&sfs=H600FW300F&langid=1033" width="100%" height="100%" scrolling="no" frameBorder="0">
+  <iframe id="widgetIframe" src="https://www.expedia.com/marketing/widgets/searchform/widget?wtt=5&lob=H,FH,F,CA,A&des=&wbi=666699&olc=FFFFFF&whf=4&hfc=FFFFFF&wif=4&ifc=&wbc=286078&wbf=4&bfc=FFFFFF&wws=2&sfs=H600FW300F&langid=1033" width="100%" height="100%" scrolling="no" frameBorder="0">
 
   </iframe>
 </div>}
--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -4,8 +4,9 @@ import UserConsumer from "../hooks/useDatos";
 const ImageSlider = () => {
 
   const [imagenArray, setImagenArray] = useState("")
-  const [current, setCurrent] = useState(1)
+  const [current, setCurrent] = useState(0)
   const [saveResultFinal, setSaveResultFinal] = UserConsumer();
+  
 
 let images = ""
 let SliderData=[]
@@ -13,25 +14,30 @@ let SliderData=[]
 
 
 useEffect(() => {
-  fetch(`https://pixabay.com/api/?key=32822302-e0bccee14336fc56618e7f358&q=sevilla+city&image_type=photo&pretty=true`)
+
+  
+  fetch(`https://pixabay.com/api/?key=32822302-e0bccee14336fc56618e7f358&q=${saveResultFinal[2]}+city&image_type=photo&pretty=true`)
   .then(response => response.json())
   .then(data =>   setImagenArray(data));
 }
 ,[]
 );
+
   
-
-
   return ( 
     <>
 
-    <div className="slider">
-   { <button className="right-arrow" onClick={()=>current == 6 ? "": setCurrent(current + 1)} > + </button>}
-   {imagenArray ? <img src={imagenArray.hits[current].largeImageURL} alt='travel image' className='image' /> 
-   : ""}
-   {   <button className="left-arrow" onClick={()=>current == 0 ? "":setCurrent(current - 1) } > - </button>}
-   </div>
+    
+{!imagenArray? "" : 
 
+  <div className="slider">
+   
+   { <button className="right-arrow" onClick={()=>current == 6 ? "": setCurrent(current + 1)} > + </button>}
+   <img src={imagenArray.hits[current].largeImageURL} alt='travel image' className='image' /> 
+   
+   {   <button className="left-arrow" onClick={()=>current == 0 ? "":setCurrent(current - 1) } > - </button>}
+  </div>
+}
  </>
   );
 }
